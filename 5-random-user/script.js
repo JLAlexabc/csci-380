263ea3677d85fe400183b7fcc858995c49fffc5f
AlexJ <linctjabc@gmail.com>
2021-02-27 11:48:08 -0500

    adding whole address information form json

--- 5-random-user/script.js
+++ 5-random-user/script.js
@@ -4,7 +4,7 @@ const avatar = document.getElementById('avatar');
 const fullname = document.getElementById('fullname');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
-const city = document.getElementById('city');
+const address = document.getElementById('address');   
 const btn = document.getElementById('btn');
 
 btn.addEventListener("click", () => {
@@ -30,6 +30,11 @@ function updateProfile(profile) {
   fullname.innerHTML = profile.results[0].name.first + " " + profile.results[0].name.last;
   username.innerHTML = profile.results[0].login.username;
   email.innerHTML = profile.results[0].email;
-  city.innerHTML = profile.results[0].location.city;
+  address.innerHTML = profile.results[0].location.street.number+" "
+  +profile.results[0].location.street.name+", "
+  +profile.results[0].location.city+", "
+  +profile.results[0].location.state+", "
+  +profile.results[0].location.country;
+  //city.innerHTML = "form the js file.";
   return 1;
 }
